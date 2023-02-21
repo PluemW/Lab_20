48beab09202825ed1070f4dbf7d0d2579a18524b
PluemW <96386038+PluemW@users.noreply.github.com>
2023-02-21 17:29:00 +0700

    Filled

--- lab20_3.cpp
+++ lab20_3.cpp
@@ -10,7 +10,10 @@ using namespace std;
 struct student{
 
     //Define struct student with four member (name ,id , gender, gpa);
-    
+    string name;
+    int id;
+    char gender;
+    float gpa;
 };
 
 struct course{
@@ -20,7 +23,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -63,9 +66,10 @@ int main(){
 	
 	while(getline(student_file,textline)){
 		student s; 
-		
+		char n[100];
 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
+        sscanf(textline.c_str(), "%[^,],%d,%c,%f", n, &s.id, &s.gender, &s.gpa);
+        s.name = n;
 		allstudents.push_back(s); 		
 	}
 	
@@ -85,7 +89,7 @@ int main(){
 			}else{
 			
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
+			    allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
@@ -94,7 +98,8 @@ int main(){
 				student *p = findstudent(allstudents,atof(textline.c_str()));
 				
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
-				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
+			    
 			}
 		}
 	}
